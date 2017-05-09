e8bf4b921dee21c9f318188ed24a06e4b9a70501
Penguinzzz <mprash27@gmail.com>
2017-05-09 09:45:24 -0400

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,18 @@
+
+public class Square {
+	protected int x;
+	protected int y;
+	protected Piece p;
+	public Square(int uX, int uY) {
+		x = uX;
+		y = uY;
+	}
+	
+	public void setPiece(Piece other) {
+		p = other;
+	}
+	
+	public Piece getPiece() {
+		return p;
+	}
+}
